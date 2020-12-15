c370ad8a0bac7f32a7daad42d8d6803edab719a7
Blair Noctis <code@noctis.im>
2020-12-15 19:58:54 +0800

    Initialize

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+use fantoccini::{ Client as FClient, Locator as FLocator};
+
+#[tokio::main]
+async fn main() -> Result<(), fantoccini::error::CmdError> {
+    let mut c = FClient::new("http://localhost:4444").await.expect("failed to connect to WebDriver");
+
+    // first, go to the Wikipedia page for Foobar
+    c.goto("https://en.wikipedia.org/wiki/Foobar").await?;
+    let url = c.current_url().await?;
+    assert_eq!(url.as_ref(), "https://en.wikipedia.org/wiki/Foobar");
+
+    // click "Foo (disambiguation)"
+    c.find(CLocator::Css(".mw-disambig")).await?.click().await?;
+
+    // click "Foo Lake"
+    c.find(CLocator::LinkText("Foo Lake")).await?.click().await?;
+
+    let url = c.current_url().await?;
+    assert_eq!(url.as_ref(), "https://en.wikipedia.org/wiki/Foo_Lake");
+
+    c.close().await
+}
